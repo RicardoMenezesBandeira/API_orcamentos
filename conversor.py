3f6ee7c3cdf3f236d1772fd068ab5b659086b941
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-04-05 16:51:19 -0300

    refactor: trocando a biblioteca de converção

--- conversor.py
+++ conversor.py
@@ -1,8 +1,22 @@
-from weasyprint import HTML
+import pdfkit
+import os
 
-# Converter o HTML para PDF
-def convert(modelo,codigo):
-    html = HTML(modelo)
-    html.write_pdf(f"{codigo}_{modelo}.pdf")
+# Exemplo de caminho no Windows
+caminho_executavel = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 
-    print("PDF gerado com sucesso: output.pdf")
+config = pdfkit.configuration(wkhtmltopdf=caminho_executavel)
+
+
+def html_para_pdf(caminho_html, caminho_pdf):
+    if not os.path.exists(caminho_html):
+        raise FileNotFoundError(f"Arquivo HTML não encontrado: {caminho_html}")
+
+    try:
+        pdfkit.from_file(caminho_html, caminho_pdf, configuration=config)
+        print(f"PDF gerado com sucesso: {caminho_pdf}")
+    except Exception as e:
+        print(f"Erro ao converter HTML para PDF: {e}")
+
+
+# Exemplo de uso
+html_para_pdf("template/boss-br.html", "template.pdf")
