a7bb532094b63f17bea5ef52746e01c77ef9d02d
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-04-10 10:06:07 -0300

    fix: corrigindo a lógica de autenticação e adicionando tratamento de tokens

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ import jwt
 import datetime
 import json
 USERS = json.load(open('bd/funcionarios.json', 'r'))  # Load users from a JSON file
-
+USERS_TOKENS=[]
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -29,7 +29,15 @@ def create_token(username, secret_key):
     }, secret_key, algorithm='HS256')
     return token
 def logon(username, password,secret_key):
-    if USERS.get(username) == password:
+    global USERS
+    if USERS.get(username)[0] == password:
         token = create_token(username,secret_key)
-        return jsonify({'token': token})
+        USERS_TOKENS.append(USERS.get(username[1], token))
+        return jsonify({'token': token,'user':USERS.get(username)[1]})
+    else:
+        USERS = json.load(open('bd/funcionarios.json', 'r'))
+        if USERS.get(username)[0] == password:
+            USERS_TOKENS.append(USERS.get(username[1], token))
+            return jsonify({'token': token,'user':USERS.get(username)[1]})
+
     return jsonify({'message': 'Credenciais inválidas'}), 401
