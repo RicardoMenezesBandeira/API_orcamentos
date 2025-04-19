8dd2a707683e292a6d670d0d9bd97a2092bf2593
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-04-18 21:27:25 -0300

    realocando com caminhos ajustados

--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,7 @@ document.querySelector(".btn").addEventListener("click", () => {
     alert("Novo orçamento será gerado aqui.");
   });
   
+  function novoOrcamento(){
+    // Redireciona para a página principal
+    window.location.href = "/preencher";
+  }
