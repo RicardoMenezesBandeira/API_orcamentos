d89ccf926e16f988a4e014bfb43b1003311a39e7
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-04-26 17:49:03 -0300

    feat: Adiciona importação de make_response para manipulação de respostas

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, redirect, render_template,send_file, send_from_directory, url_for
+from flask import Flask, request, jsonify, redirect, render_template,send_file, send_from_directory, url_for,make_response
 from werkzeug.utils import secure_filename  # sanitiza nomes de arquivos
 from flask_cors import CORS
 import os
