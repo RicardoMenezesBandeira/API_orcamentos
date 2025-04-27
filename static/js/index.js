ceaaf431ce9d8d48d57cbd273c73b05e1dd9535e
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-04-27 14:12:30 -0300

    feat: Remove alerta de novo orçamento e organiza a função de atualização de orçamento

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,3 @@
-document.querySelector(".btn").addEventListener("click", () => {
-    alert("Novo orçamento será gerado aqui.");
-  });
   
   function novoOrcamento(){
     // Redireciona para a página principal
@@ -9,4 +6,32 @@ document.querySelector(".btn").addEventListener("click", () => {
   function novoFuncionario(){
     // Redireciona para a página principal
     window.location.href = "/cadastro";
+  }
+  function atualizaOrcamento(){
+    fetch('/orçaemnto', {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Accept': 'application/json',
+        // Inclua o token de autenticação se necessário
+        // 'Authorization': 'Bearer SEU_TOKEN_AQUI'
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição: ' + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Dados recebidos:', data);
+      // Aqui você pode manipular os dados recebidos, como atualizar o DOM
+    })
+    .catch(error => {
+      console.error('Erro ao buscar os dados:', error);
+    });
+    }
+  
+  window.onload = function() {
+    atualizaOrcamento();
   }
