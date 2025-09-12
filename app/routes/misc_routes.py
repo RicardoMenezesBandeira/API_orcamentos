ead5a08162fb388e51f2f19c1d878c106a23d2a3
Caio Dias Ferreira <cai0ferreira@hotmail.com>
2025-09-11 23:49:18 -0300

    feat: add search input for budget number in dashboard and index template

--- app/routes/misc_routes.py
+++ app/routes/misc_routes.py
@@ -12,7 +12,7 @@ def get_dashboard(user_data):
     dados =   get_data(user)
     nome = dados.get("nome")
     info     = f"Nome: {nome}"
-    btn = "<button class='btn' onclick='novoOrcamento()'>gerar novo orçamento</button>"
+    btn = "<input type='text' class='search' placeholder='Buscar Nº de orçamento...' oninput='filtrarOrcamentos(this.value)'><button class='btn' onclick='novoOrcamento()'>gerar novo orçamento</button>"
     if dados.get("admin"):
         btn += " <button class='btn' onclick='novoFuncionario()'>cadastra empregado</button>"
 
